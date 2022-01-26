0ac2565d3b5a264acdd4e5d6a608a57c93cec41f
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-26 11:58:24 -0600

    first

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var isSucc = false;
+            var nOfWheels = 0;
+            while (!isSucc)
+            {
+                Console.WriteLine("How many wheels does it have?");
+                isSucc = int.TryParse(Console.ReadLine(), out nOfWheels);
+            }
+
+            IVehicle vehicle = VehicleFactory.GetVehicle(nOfWheels);
+            vehicle.Drive();
+        }
+    }
+}
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheels)
+        {
+            switch (wheels)
+            {
+                case 2:
+                    return new Motorcycle();
+                case 4:
+                    return new Car();
+                default:
+                    return new Car();
+            }
+        }
+    }
+}
